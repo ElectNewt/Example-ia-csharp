0aba20e25d8eb4f60e934a0840cedbd70289e95a
Netmentor <ivanxo69@gmail.com>
2025-09-07 15:24:54 +0100

    video 01 - how to run IA locally with C#

---
+++ src/01_run_locally/OLlamaExample.cs
@@ -0,0 +1,27 @@
+﻿using OllamaSharp;
+
+namespace ExampleIA01;
+
+public class OLlamaExample
+{
+    public async Task Execute()
+    {
+        Uri uri = new Uri("http://localhost:11434");
+        OllamaApiClient ollama = new OllamaApiClient(uri);
+
+        // select a model which should be used for further operations
+        ollama.SelectedModel = "llama3";
+        
+        Chat chat = new Chat(ollama);
+
+        while (true)
+        {
+            Console.Write(">>");
+            string? message = Console.ReadLine();
+            await foreach (string answerToken in chat.SendAsync(message))
+                Console.Write(answerToken);
+            
+            Console.WriteLine();
+        }
+    }
+}
---
+++ src/01_run_locally/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ExampleIA01;
+
+// OLlamaExample localAiExample = new OLlamaExample();
+DirectLlamaExample localAiExample = new DirectLlamaExample();
+await localAiExample.Execute();
